38348c4547987663a409294ddb036a64b01bf21b
joakim-klemetsen <joakim.klemetsen@icloud.com>
2023-10-14 18:32:24 +0200

    Creating the vol_qry function

--- gql-queries/vol_qry.r
+++ gql-queries/vol_qry.r
@@ -1,21 +1,24 @@
 # Function returning hourly traffic volumes for a given station (id) for a 
 # given time interval [from, to].
-vol_qry <- function(id, from, to){
-  trafficData(trafficRegistrationPointId: id) {
-    volume {
-      byHour(from: from, to: to) {
-        edges {
-          node {
-            from
-            to
-            total {
-              volumeNumbers {
-                volume
+vol_qry <- function(id, from, to) {
+  query <- sprintf('{
+    trafficData(trafficRegistrationPointId: "%s") {
+      volume {
+        byHour(from: "%s", to: "%s") {
+          edges {
+            node {
+              from
+              to
+              total {
+                volumeNumbers {
+                  volume
+                }
               }
             }
           }
         }
       }
     }
-  }
+  }', id, from, to)
+  return(query)
 }
