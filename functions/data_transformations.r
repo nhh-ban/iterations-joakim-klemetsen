16643c2e70ba20dc5d46277c4578acff55723c0b
joakim-klemetsen <joakim.klemetsen@icloud.com>
2023-10-15 11:32:47 +0200

    Creating 'transform_volumes' function + minor changes to the script

--- functions/data_transformations.r
+++ functions/data_transformations.r
@@ -16,10 +16,33 @@ transform_metadata_to_df <- function(data){
 # Function returning a data time variable in ISO8601 format, with added
 # offset.
 to_iso8601 <- function(datetime, offset){
+  # Ensure that datetime is correctly parsed as a datetime object
   datetime <- as_datetime(datetime, tz = "UTC")
-  offset_datetime <- datetime + days(offset) 
-  time <- paste0(iso8601(anytime(offset_datetime)), "Z")
+  
+  # Check if the conversion was successful before proceeding
+  if(is.na(datetime)) stop("Datetime conversion failed for input: ", datetime)
+  
+  offset_datetime <- datetime + lubridate::days(offset) 
+  
+  # Ensure the offset does not create an NA
+  if(is.na(offset_datetime)) stop("Offset results in an invalid datetime for input: ", datetime)
+  
+  # Return the datetime in ISO8601 format with "Z" denoting UTC time
+  time <- paste0(anytime::iso8601(offset_datetime), "Z")
   return(time)
+}
+
+# Function transforming query return to a data frame suitable for plotting 
+transform_volumes <- function(query_output) {
+  query_output %>% 
+    pluck("trafficData", "volume", "byHour", "edges") %>% 
+    map_dfr(~{
+      tibble(
+        from = anytime::anytime(.x$node$from),
+        to = anytime::anytime(.x$node$to),
+        volume = .x$node$total$volumeNumbers$volume
+      )
+    })
 }
 
 
